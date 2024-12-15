303f6d1c9d67587974efa539a0864105619a7886
Aurko Datta <148306612+AurkoDatta@users.noreply.github.com>
2024-12-14 20:40:18 -0500

    Initial commit

---
+++ src/main/java/org/example/num1768.java
@@ -0,0 +1,38 @@
+package org.example;
+
+public class num1768 {
+    public static String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder();
+
+        if (word1.length() == word2.length()) {
+            for (int i = 0; i < word1.length(); i++) {
+                sb.append(word1.charAt(i));
+                sb.append(word2.charAt(i));
+            }
+        } else if (word1.length() > word2.length()) {
+            for (int i = 0; i < word1.length(); i++) {
+                if (i < word2.length()) {
+                    sb.append(word1.charAt(i));
+                    sb.append(word2.charAt(i));
+                } else {
+                    sb.append(word1.charAt(i));
+                }
+            }
+        } else if (word1.length() < word2.length()) {
+            for (int i = 0; i < word2.length(); i++) {
+                if (i < word1.length()) {
+                    sb.append(word1.charAt(i));
+                    sb.append(word2.charAt(i));
+                } else {
+                    sb.append(word2.charAt(i));
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(mergeAlternately("ab", "pqrs"));
+    }
+}
+
---
+++ src/main/java/org/example/num2239.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class num2239 {
+    public int findClosestNumber(int[] nums) {
+        int result = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (Math.abs(nums[i]) <= Math.abs(result)) {
+                if (Math.abs(nums[i]) == result){
+                    result = Math.max(nums[i], result);
+                }else {
+                    result = nums[i];
+                }
+            }
+
+        }
+        return result;
+    }
+}
