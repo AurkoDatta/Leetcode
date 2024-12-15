e4952cfd1b7b951d6406e75cf7a0958ca6d84036
Aurko Datta <148306612+AurkoDatta@users.noreply.github.com>
2024-12-15 17:52:44 -0500

    algomap 5 incomplete

--- src/main/java/org/LeetCodeSols/num121.java
+++ src/main/java/org/LeetCodeSols/num121.java
@@ -1,4 +1,30 @@
 package org.LeetCodeSols;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class num121 {
+    public static int maxProfit(int[] prices) {
+        int result = 0;
+        int temp1 = Arrays.stream(prices).min().getAsInt();
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] == temp1){
+                for (int j = i; j < prices.length; j++) {
+                    int profit = prices[j] - temp1;
+                    result = Math.max(result, profit);
+
+                }
+                break;
+            }
+        }
+
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{1,2}));
+    }
 }
