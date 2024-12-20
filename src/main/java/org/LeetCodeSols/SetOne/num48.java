55c5b737576f67ba90fc23fae072358b2e5d4e14
AurkoDatta <dattaa18@mcmaster.ca>
2024-12-20 16:22:13 -0500

    Algomap 1st set complete

---
+++ src/main/java/org/LeetCodeSols/SetOne/num48.java
@@ -0,0 +1,26 @@
+package org.LeetCodeSols.SetOne;
+
+public class num48 {
+    public static void rotate(int[][] matrix) {
+
+        //Tranpose
+        int n = matrix.length;
+        for (int i = 0; i < n ; i++) {
+            for (int j = i+1; j < n ; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        //Reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n/2 ; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 -j];
+                matrix[i][n - 1 -j] = temp;
+            }
+        }
+
+    }
+}
