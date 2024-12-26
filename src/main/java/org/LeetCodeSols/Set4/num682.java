4951b2ac0a70b66cdf671d645b675b12846b02c1
AurkoDatta <dattaa18@mcmaster.ca>
2024-12-26 14:53:42 +0600

    Set4 Q1

---
+++ src/main/java/org/LeetCodeSols/Set4/num682.java
@@ -0,0 +1,46 @@
+package org.LeetCodeSols.Set4;
+
+import java.util.Stack;
+
+public class num682 {
+    public static int calPoints(String[] operations) {
+
+        int result = 0;
+
+        Stack<Integer> stack = new Stack<>();
+        for (String op : operations) {
+            if (op.equals("+")) {
+                int value = stack.get((stack.size() - 1)) + (stack.get(stack.size() - 2));
+                stack.push(value);
+            } else if (op.equals("D")) {
+                stack.push(stack.get(stack.size() - 1) * 2);
+            } else if (op.equals("C")) {
+                stack.pop();
+            } else {
+                int currentNumber = Integer.parseInt(op);
+                stack.push(currentNumber);
+            }
+        }
+
+        for (int i : stack) {
+            result += i;
+        }
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        String[] test = {"5","2","C","D","+"};
+        System.out.println(calPoints(test));
+
+//        Stack<Integer> stack = new Stack<>();
+//        stack.push(5);
+//        stack.push(2);
+//        stack.push(1);
+//        stack.pop();
+//        System.out.println(stack);
+
+    }
+
+}
