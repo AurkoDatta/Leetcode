b779a05733d3b517a859b3a1ea39b351a02c42e3
Aurko Datta <148306612+AurkoDatta@users.noreply.github.com>
2025-01-08 15:00:33 -0500

    Delete duplicates in Linked List, num83

---
+++ src/main/java/org/LeetCodeSols/LinkedList/num83.java
@@ -0,0 +1,34 @@
+package org.LeetCodeSols.LinkedList;
+
+public class num83 {
+
+    public class ListNode {
+
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+
+    public static ListNode deleteDuplicates(ListNode head) {
+
+        ListNode current = head;
+
+        while (head != null && head.next != null) {
+            if (head.val == head.next.val) {
+                head.next = head.next.next;
+            } else {
+                head = head.next;
+            }
+        }
+
+        return current;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
