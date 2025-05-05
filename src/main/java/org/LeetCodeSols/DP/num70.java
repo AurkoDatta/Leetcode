39f778dfb96993b586e3fbb0a39c5f3462c84efe
Aurko Datta <148306612+AurkoDatta@users.noreply.github.com>
2025-05-05 11:02:24 -0400

    Improved the Climbing Stairs problem

--- src/main/java/org/LeetCodeSols/DP/num70.java
+++ src/main/java/org/LeetCodeSols/DP/num70.java
@@ -19,4 +19,22 @@ public class num70 {
         return res;
 
     }
+
+    public int climbStairsFaster(int n) {
+        int dp[] = new int[n+1];
+
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            if (i == 1){
+                dp[i] = dp[i-1] + 0;
+            }
+            else {
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+        }
+
+        return dp[n];
+
+    }
 }
