0ad9e788cbf593dbb5ea842b82f07bc6d8570d21
Aurko Datta <148306612+AurkoDatta@users.noreply.github.com>
2026-08-18 18:11:36 +0000

    notes for num42 trapping rain water

--- src/main/java/org/LeetCodeSols/TwoPointer/num42.java
+++ src/main/java/org/LeetCodeSols/TwoPointer/num42.java
@@ -1,5 +1,11 @@
 package org.LeetCodeSols.TwoPointer;
 
+/***
+ * two pointers starting from each end, track the max height seen so far on each side
+ * water trapped at a spot is the smaller of the two max heights minus the height there
+ * move whichever side has the smaller max, that side is the one that still needs handling
+ */
+
 public class num42 {
     public int trap(int[] height) {
         int res = 0;
