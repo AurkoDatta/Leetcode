bb60cf8e9515e491ae7c6833297ba30164b8159a
Aurko Datta <148306612+AurkoDatta@users.noreply.github.com>
2026-08-20 18:11:15 +0000

    Added explanation for num206

--- src/main/java/org/LeetCodeSols/LinkedList/num206.java
+++ src/main/java/org/LeetCodeSols/LinkedList/num206.java
@@ -1,5 +1,12 @@
 package org.LeetCodeSols.LinkedList;
 
+/***
+ * walk through the list flipping each next pointer to face backwards
+ * keep a prev pointer that trails behind as the new front of the reversed part
+ * save the next node before flipping so we dont lose the rest of the list
+ * once curr falls off the end prev is the new head
+ */
+
 public class num206 {
     public class ListNode {
         int val;
