aa4f8d770f15d56b17c94f88449b0aa9c4a6cfb0
Aurko Datta <148306612+AurkoDatta@users.noreply.github.com>
2026-08-22 18:10:40 +0000

    Reorder list num143

---
+++ src/main/java/org/LeetCodeSols/LinkedList/num143.java
@@ -0,0 +1,54 @@
+package org.LeetCodeSols.LinkedList;
+
+/***
+ * find the middle of the list with a slow and fast pointer
+ * reverse everything after that middle node
+ * then zip the front half and the reversed back half together one node at a time
+ */
+
+public class num143 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static void reorderList(ListNode head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode second = slow.next;
+        slow.next = null;
+
+        ListNode prev = null;
+        while (second != null) {
+            ListNode next = second.next;
+            second.next = prev;
+            prev = second;
+            second = next;
+        }
+        second = prev;
+
+        ListNode first = head;
+        while (second != null) {
+            ListNode firstNext = first.next;
+            ListNode secondNext = second.next;
+
+            first.next = second;
+            second.next = firstNext;
+
+            first = firstNext;
+            second = secondNext;
+        }
+    }
+}
