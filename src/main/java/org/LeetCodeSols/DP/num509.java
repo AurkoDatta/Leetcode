b331c49e5e4dc7013c6904e1ca8e5c3603db5765
Aurko Datta <148306612+AurkoDatta@users.noreply.github.com>
2026-08-22 18:10:40 +0000

    explanation for num509

--- src/main/java/org/LeetCodeSols/DP/num509.java
+++ src/main/java/org/LeetCodeSols/DP/num509.java
@@ -1,5 +1,12 @@
 package org.LeetCodeSols.DP;
 
+/***
+ * straight up recursive version, no memo here
+ * base case is n is 0 or 1, just return n itself
+ * otherwise its the sum of the previous two fib calls
+ * works fine but recomputes the same subproblems over and over, v2 fixes that
+ */
+
 public class num509 {
     public static int fib(int n) {
          if (n <= 1){
