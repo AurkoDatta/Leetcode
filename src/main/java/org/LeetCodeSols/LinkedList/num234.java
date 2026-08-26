8097ed5a0011ee007fcc49c2175214520eeefe22
Aurko Datta <148306612+AurkoDatta@users.noreply.github.com>
2026-08-26 18:13:12 +0000

    Palindrome Linked List num234

---
+++ src/main/java/org/LeetCodeSols/LinkedList/num234.java
@@ -0,0 +1,52 @@
+package org.LeetCodeSols.LinkedList;
+
+/***
+ * find the middle using slow and fast pointers
+ * reverse the second half in place
+ * walk the first half and the reversed second half together comparing values
+ * if anything doesnt match its not a palindrome
+ */
+
+public class num234 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode prev = null;
+        ListNode curr = slow;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        ListNode first = head;
+        ListNode second = prev;
+        while (second != null) {
+            if (first.val != second.val) {
+                return false;
+            }
+            first = first.next;
+            second = second.next;
+        }
+
+        return true;
+    }
+}
