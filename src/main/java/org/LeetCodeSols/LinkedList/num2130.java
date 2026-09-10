c38126e03308c97d2a9a87f1376bf1388f569920
Aurko Datta <148306612+AurkoDatta@users.noreply.github.com>
2026-09-10 19:05:19 +0000

    num2130 maximum twin sum of a linked list

---
+++ src/main/java/org/LeetCodeSols/LinkedList/num2130.java
@@ -0,0 +1,40 @@
+package org.LeetCodeSols.LinkedList;
+
+public class num2130 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static int pairSum(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode prev = null;
+        ListNode curr = slow;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        int max = 0;
+        ListNode first = head;
+        ListNode second = prev;
+        while (second != null) {
+            max = Math.max(max, first.val + second.val);
+            first = first.next;
+            second = second.next;
+        }
+
+        return max;
+    }
+}
