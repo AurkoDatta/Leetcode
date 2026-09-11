1bef3fb2d22d1ba09ec54176c9f7f9e9eb948552
Aurko Datta <148306612+AurkoDatta@users.noreply.github.com>
2026-09-11 16:40:13 +0000

    num49 group anagrams

---
+++ src/main/java/org/LeetCodeSols/HashMaps/num49.java
@@ -0,0 +1,24 @@
+package org.LeetCodeSols.HashMaps;
+
+import java.util.*;
+
+public class num49 {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+}
