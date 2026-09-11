f22982c53296e862d990f31ae44a39e8d102ce7c
Aurko Datta <148306612+AurkoDatta@users.noreply.github.com>
2026-09-11 16:40:23 +0000

    explanation for num735

--- src/main/java/org/LeetCodeSols/Stacks/num735.java
+++ src/main/java/org/LeetCodeSols/Stacks/num735.java
@@ -2,6 +2,16 @@ package org.LeetCodeSols.Stacks;
 
 import java.util.Stack;
 
+/***
+ * walk through the asteroids left to right, keeping a stack of the ones still moving right
+ * a right moving asteroid always just gets pushed, nothing to collide with yet
+ * when a left moving one shows up compare it against whatever is on top of the stack
+ * smaller one on top gets popped and we keep checking the next one down
+ * same size means they both blow up, bigger one on top means the new asteroid is the one that explodes
+ * if it survives all the way through it gets pushed on too
+ * whatever is left in the stack at the end is the final state, read bottom to top
+ */
+
 public class num735 {
     public static int[] asteroidCollision(int[] asteroids) {
         Stack<Integer> stack = new Stack<>();
