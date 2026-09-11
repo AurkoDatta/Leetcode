af7e814e07b2e72a6449cdad6befde92de282efb
Aurko Datta <148306612+AurkoDatta@users.noreply.github.com>
2026-09-11 16:40:08 +0000

    num142 linked list cycle II

---
+++ src/main/java/org/LeetCodeSols/LinkedList/num142.java
@@ -0,0 +1,33 @@
+package org.LeetCodeSols.LinkedList;
+
+public class num142 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public static ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                ListNode ptr = head;
+                while (ptr != slow) {
+                    ptr = ptr.next;
+                    slow = slow.next;
+                }
+                return ptr;
+            }
+        }
+
+        return null;
+    }
+}
