4913788febc81dcd2137566b772cd7119542c226
Aurko Datta <148306612+AurkoDatta@users.noreply.github.com>
2026-09-12 16:40:07 +0000

    num239 sliding window maximum

---
+++ src/main/java/org/LeetCodeSols/SlidingWindow/num239.java
@@ -0,0 +1,30 @@
+package org.LeetCodeSols.SlidingWindow;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class num239 {
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!deque.isEmpty() && deque.peekFirst() < i - k + 1) {
+                deque.pollFirst();
+            }
+
+            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
+                deque.pollLast();
+            }
+
+            deque.offerLast(i);
+
+            if (i >= k - 1) {
+                result[i - k + 1] = nums[deque.peekFirst()];
+            }
+        }
+
+        return result;
+    }
+}
