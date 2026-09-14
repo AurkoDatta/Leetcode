984cd5165223e06e2715f8e953de0031d7e165f4
Aurko Datta <148306612+AurkoDatta@users.noreply.github.com>
2026-09-14 16:39:23 +0000

    num139 word break

---
+++ src/main/java/org/LeetCodeSols/DP/num139.java
@@ -0,0 +1,24 @@
+package org.LeetCodeSols.DP;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class num139 {
+    public static boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> words = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && words.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[s.length()];
+    }
+}
