447f9e5fca4821304b0147a88022318cb9e36a1a
AurkoDatta <aurkodatta08@gmail.com>
2026-09-16 17:56:26 +0000

    num560 subarray sum equals k

---
+++ src/main/java/org/LeetCodeSols/HashMaps/num560.java
@@ -0,0 +1,22 @@
+package org.LeetCodeSols.HashMaps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class num560 {
+    public static int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        int sum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            sum += num;
+            count += map.getOrDefault(sum - k, 0);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+}
