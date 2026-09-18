ed999600dffb085985b0b223b1c5a1cea2a362e0
AurkoDatta <aurkodatta08@gmail.com>
2026-09-18 16:40:27 +0000

    num55 jump game

---
+++ src/main/java/org/LeetCodeSols/Arrays/num55.java
@@ -0,0 +1,16 @@
+package org.LeetCodeSols.Arrays;
+
+public class num55 {
+    public static boolean canJump(int[] nums) {
+        int reach = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > reach) {
+                return false;
+            }
+            reach = Math.max(reach, i + nums[i]);
+        }
+
+        return true;
+    }
+}
