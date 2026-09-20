6a4e98174e7634db1d0fd3337ec8bf735a17e7f8
AurkoDatta <aurkodatta08@gmail.com>
2026-09-20 16:43:54 +0000

    num16 3sum closest

---
+++ src/main/java/org/LeetCodeSols/TwoPointer/num16.java
@@ -0,0 +1,30 @@
+package org.LeetCodeSols.TwoPointer;
+
+import java.util.Arrays;
+
+public class num16 {
+    public static int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int closest = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int j = i + 1, k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (Math.abs(sum - target) < Math.abs(closest - target)) {
+                    closest = sum;
+                }
+
+                if (sum == target) {
+                    return sum;
+                } else if (sum < target) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+
+        return closest;
+    }
+}
