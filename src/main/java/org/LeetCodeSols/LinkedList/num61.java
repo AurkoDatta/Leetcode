153627393c637f556433112493f5a06d39f16652
AurkoDatta <aurkodatta08@gmail.com>
2026-09-24 16:42:33 +0000

    num61 rotate list

---
+++ src/main/java/org/LeetCodeSols/LinkedList/num61.java
@@ -0,0 +1,37 @@
+package org.LeetCodeSols.LinkedList;
+
+public class num61 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0) return head;
+
+        int length = 1;
+        ListNode tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+            length++;
+        }
+
+        int shift = k % length;
+        if (shift == 0) return head;
+
+        tail.next = head;
+        int stepsToNewTail = length - shift;
+        ListNode newTail = tail;
+        for (int i = 0; i < stepsToNewTail; i++) {
+            newTail = newTail.next;
+        }
+
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+
+        return newHead;
+    }
+}
