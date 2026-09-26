987da39995c6b5efe33d8bf74d6d5043d4b74186
AurkoDatta <aurkodatta08@gmail.com>
2026-09-26 16:44:44 +0000

    num232 implement queue using stacks

---
+++ src/main/java/org/LeetCodeSols/Stacks/num232.java
@@ -0,0 +1,35 @@
+package org.LeetCodeSols.Stacks;
+
+import java.util.Stack;
+
+public class num232 {
+    private Stack<Integer> inStack;
+    private Stack<Integer> outStack;
+
+    public num232() {
+        inStack = new Stack<>();
+        outStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        inStack.push(x);
+    }
+
+    public int pop() {
+        peek();
+        return outStack.pop();
+    }
+
+    public int peek() {
+        if (outStack.isEmpty()) {
+            while (!inStack.isEmpty()) {
+                outStack.push(inStack.pop());
+            }
+        }
+        return outStack.peek();
+    }
+
+    public boolean empty() {
+        return inStack.isEmpty() && outStack.isEmpty();
+    }
+}
